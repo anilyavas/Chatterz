f1fb319a28417de94d907826eb4b71b662ec5caa
Anıl <anil@Anl-MacBook-Air.local>
2024-02-20 13:42:39 +0300

    chatscreen top comp

---
+++ components/ChatTopContainer.js
@@ -0,0 +1,71 @@
+import { View, Text, Pressable, Image, StyleSheet } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import { colors } from '../constants/colors';
+import { Ionicons, Feather, AntDesign } from '@expo/vector-icons';
+import { dummyData } from '../assets/data/dummyData';
+
+const ChatTopContainer = () => {
+  const navigation = useNavigation();
+  return (
+    <View style={styles.topContainer}>
+      <Pressable style={styles.arrow} onPress={() => navigation.goBack()}>
+        <Ionicons name='arrow-back' size={30} color={colors.snow} />
+      </Pressable>
+      <Image source={{ uri: dummyData[1].imageLink }} style={styles.image} />
+      <View style={styles.nameContainer}>
+        <Text style={styles.name}>
+          {dummyData[1].name} {dummyData[1].surname}
+        </Text>
+        <Text style={styles.status}>Active Now</Text>
+      </View>
+      <View style={{ flex: 1 }} />
+      <View style={styles.callContainer}>
+        <Pressable>
+          <Feather name='phone-call' color={colors.snow} size={30} />
+        </Pressable>
+        <Pressable>
+          <AntDesign name='videocamera' size={30} color={colors.snow} />
+        </Pressable>
+      </View>
+    </View>
+  );
+};
+
+export default ChatTopContainer;
+
+const styles = StyleSheet.create({
+  topContainer: {
+    borderBottomWidth: 1,
+    borderBottomColor: colors.grey,
+    flexDirection: 'row',
+    padding: 10,
+  },
+  nameContainer: {
+    paddingLeft: 15,
+    paddingTop: 5,
+  },
+  callContainer: {
+    flexDirection: 'row',
+    gap: 20,
+    alignItems: 'center',
+  },
+  arrow: {
+    padding: 10,
+    justifyContent: 'center',
+  },
+  image: {
+    width: 60,
+    height: 60,
+    borderRadius: 30,
+  },
+  name: {
+    fontFamily: 'Poppins-SemiBold',
+    color: colors.snow,
+    fontSize: 16,
+  },
+  status: {
+    fontFamily: 'Poppins-Regular',
+    color: colors.grey,
+    fontSize: 14,
+  },
+});
--- screens/ChatScreen.js
+++ screens/ChatScreen.js
@@ -1,11 +1,31 @@
-import { View, Text } from 'react-native';
-
+import {
+  View,
+  Text,
+  SafeAreaView,
+  StyleSheet,
+  Pressable,
+  Image,
+} from 'react-native';
+import { colors } from '../constants/colors';
+import { Ionicons } from '@expo/vector-icons';
+import { useNavigation } from '@react-navigation/native';
+import { dummyData } from '../assets/data/dummyData';
+import ChatTopContainer from '../components/ChatTopContainer';
 const ChatScreen = () => {
+  const navigation = useNavigation();
   return (
-    <View>
+    <SafeAreaView style={styles.container}>
+      <ChatTopContainer />
       <Text>ChatScreen</Text>
-    </View>
+    </SafeAreaView>
   );
 };
 
 export default ChatScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: colors.black,
+  },
+});
